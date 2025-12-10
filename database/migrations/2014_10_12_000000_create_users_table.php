0377385e69036fa8f94cca8e3fcf29d125be3dc6
YujiShibu <colacolaqwe@gmail.com>
2025-12-11 00:26:05 +0900

    テーブルの修正

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -11,26 +11,30 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id()->index();
-            $table->string('name')->index(); //名前
-            $table->string('email')->index(); //メールアドレス
-            $table->timestamp('email_verified_at')->default(null); //メールアドレス確認日時
-            $table->string('password'); //パスワード
-            $table->integer('age'); //年齢
-            $table->string('address')->default(null); //住所
-            $table->string('phone')->default(null); //電話番号
-            $table->bigInteger('group'); //部署
-            $table->bigInteger('role'); //役職
-            $table->bigInteger('sales_office'); //営業所
-            $table->timestamp('start_day')->nullable(); //入社日
-            $table->string('remember_token')->default(null); //保持トークン
-            $table->string('spare1'); //予備1
-            $table->string('spare2'); //予備2
-            $table->integer('spare3'); //予備3
-            $table->integer('spare4'); //予備4
-            $table->timestamps();
-        });
+Schema::create('users', function (Blueprint $table) {
+    $table->id()->index();
+    $table->string('name')->index();    // 名前
+    $table->string('email')->unique();  // メールアドレス
+    $table->timestamp('email_verified_at')->nullable(); // 確認日時
+    $table->string('password');  // パスワード
+    $table->integer('age');      // 年齢
+    $table->string('address')->nullable(); // 住所
+    $table->string('phone')->nullable();   // 電話番号
+    $table->unsignedBigInteger('group'); // 部署
+    $table->unsignedBigInteger('role');  // 役職
+    $table->unsignedBigInteger('sales_office'); // 営業所
+    $table->timestamp('start_day')->nullable(); // 入社日
+    $table->string('remember_token')->nullable(); // トークン
+
+    // 予備
+    $table->string('spare1')->nullable();
+    $table->string('spare2')->nullable();
+    $table->integer('spare3')->nullable();
+    $table->integer('spare4')->nullable();
+
+    $table->timestamps();
+});
+
     }
 
     /**

b51156a2c83a39fe1550ff3655157fb24a0f55ee
nishinaka <nmomomo412@gmail.com>
2025-12-18 17:01:52 +0900

    社員一覧画面と社員編集画面をemployeesからuserに移行しました。
    git status

--- routes/web.php
+++ routes/web.php
@@ -1,36 +1,33 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\EmployeeController;
-
-Route::resource('employees', EmployeeController::class);
-
-
 use App\Http\Controllers\ItemController;
 use App\Http\Controllers\ReviewController;
-use App\Http\Controllers\UserController;
-
+use App\Http\Controllers\AttendanceController;
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
 */
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+// ユーザー
 Route::resource('users', UserController::class);
-Route::get('/item', [App\Http\Controllers\ItemController::class, 'create']);
-Route::post('/item',[App\Http\Controllers\ItemController::class, 'store']);
+
+// 社員（既存）
+Route::resource('employees', EmployeeController::class);
 
 // 商品関連
+Route::get('/item', [ItemController::class, 'create']);
+Route::post('/item', [ItemController::class, 'store']);
+
 Route::get('/items', [ItemController::class, 'index'])->name('items.index');
 Route::get('/items/create', [ItemController::class, 'create'])->name('items.create');
 Route::post('/items', [ItemController::class, 'store'])->name('items.store');
@@ -44,3 +41,10 @@ Route::get('/items/{item}/reviews', [ReviewController::class, 'index'])->name('i
 Route::post('/items/{item}/reviews', [ReviewController::class, 'store'])->name('items.reviews.store');
 Route::get('/reviews/{review}', [ReviewController::class, 'show'])->name('reviews.show');
 
+// 出退勤関連
+Route::get('/attendances/create', [AttendanceController::class, 'create'])->name('attendances.create');
+Route::post('/attendances/clock-in', [AttendanceController::class, 'clockIn'])->name('attendances.clockIn');
+Route::post('/attendances/break-start', [AttendanceController::class, 'breakStart'])->name('attendances.breakStart');
+Route::post('/attendances/break-end', [AttendanceController::class, 'breakEnd'])->name('attendances.breakEnd');
+Route::post('/attendances/clock-out', [AttendanceController::class, 'clockOut'])->name('attendances.clockOut');
+Route::get('/attendances', [AttendanceController::class, 'index'])->name('attendances.index');

10a3cb8103c9a65d11e8e9484bd51e8b48f9b861
nishinaka <nmomomo412@gmail.com>
2025-12-22 15:11:45 +0900

    Fix: use Bootstrap CDN for Heroku

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,10 +6,11 @@
 
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
 </head>
-<body>
-    <div class="container mt-4">
-        @yield('content')
-    </div>
+<body class="bg-light">
+
+    @yield('content')
+
 </body>
 </html>
